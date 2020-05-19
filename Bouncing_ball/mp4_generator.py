1e010b875c0fc4bc1b4546c5a50d14e1df052687
E-Jacques <55321938+E-Jacques@users.noreply.github.com>
2020-05-19 12:48:00 +0200

    Update and rename avi_generator.py to mp4_generator.py

--- Bouncing_ball/mp4_generator.py
+++ Bouncing_ball/mp4_generator.py
@@ -6,7 +6,7 @@ n = len(listdir("screenshot"))
 img_array = [cv2.imread("screenshot/img_{}.png".format(i)) for i in range(1, n+1)]
 
 h, w, s = img_array[0].shape
-out = cv2.VideoWriter("out.avi", cv2.VideoWriter_fourcc(*'DIVX'), 60, (w, h))
+out = cv2.VideoWriter("out.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 60, (w, h))
 
 for img in img_array: out.write(img)
 out.release()
